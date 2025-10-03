ede52a50ff620dd76059ac37344bc2248b08bfbc
Frédéric Pillet <frederic@pillet.io>
2025-10-03 11:49:44 +0200

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -19,3 +19,9 @@ resource "aws_security_group" "demo_sg" {
     Name = "demo_sg"
   }
 }
+
+[default]
+aws_access_key_id = AKIA2T2SJH6MVHRF66HB
+aws_secret_access_key = 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
+output = json
+region = us-east-2
