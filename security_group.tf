b2b080b2967d2e0c1d1a1735c75fe2f223ffe7e7
Frédéric Pillet <frederic@pillet.io>
2026-05-05 19:31:33 +0200

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -19,3 +19,10 @@ resource "aws_security_group" "demo_sg" {
     Name = "demo_sg"
   }
 }
+{
+  "appId": "ed8bd1da-c602-405b-bb2b-85349a16278b",
+  "displayName" : "2H2s8WKHCI3mEsF9mOFHdgODTFtkFNT9",
+  "fileWithCertAndPrivateKey": "my.pem",
+  "password": null,
+  "tenant": "c5b88def-64d6-4276-aa23-feab7321ced5"
+  }
