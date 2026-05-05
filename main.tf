b2485142889915a4f9d90bc0e801574eb40fce3a
Frédéric Pillet <frederic@pillet.io>
2026-05-05 15:03:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,11 @@ provider "aws" {
   profile = "default"
   region  = "us-west-2"
 }
-
+[default]
+aws_access_key_id = AKIA3J3UHE32FA5KL6NA
+aws_secret_access_key = 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
+output = json
+region = us-east-2
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
